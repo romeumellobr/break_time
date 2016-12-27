d8153e7ddaf7ad5cd41a99641e637bd494fd08ed
romeumello@icloud.com <romeumello@icloud.com>
2016-12-27 02:50:12 -0200

    Rename files

--- break_time.py
+++ break_time.py
@@ -1,7 +1,15 @@
 import webbrowser
 import time
 
-#Delay 10 seconds
-time.sleep(10)
-#Open Sara Niemietz YouTube Channel
-webbrowser.open("https://www.youtube.com/channel/UC9E6r0GuQ7d6G6esolVc5gg")
+#set variables
+loops = 3
+counter = 0
+
+#print time.ctime()
+print("This program started on %s" % time.ctime())
+while(counter < loops ):
+    #Delay 10 seconds
+    time.sleep(10)
+    #Open Sara Niemietz YouTube Channel
+    webbrowser.open("https://www.youtube.com/channel/UC9E6r0GuQ7d6G6esolVc5gg")
+    counter = counter + 1

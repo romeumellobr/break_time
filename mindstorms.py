b5d21506fb321da25d56b5ac87546202b0d7413d
romeumello@icloud.com <romeumello@icloud.com>
2016-12-28 19:01:31 -0200

    Turtle running in circles it's a very very, mad world

--- mindstorms.py
+++ mindstorms.py
@@ -1,20 +1,26 @@
 import turtle
+import random
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+def draw_circle(some_turtle):
+    randCircle = random.randrange(-100, 100)
+    some_turtle.circle(randCircle)
+        
+def draw_art():
     window = turtle.Screen()
-    window.bgcolor("white")
-
-    pat = turtle.Turtle()
-    pat.shape("turtle")
-    pat.color("red")
-    pat.speed(1)
-    patCounter = 0
-    patPasses = 3
-    while(patCounter <= patPasses):
-        pat.forward(100)
-        pat.right(90)
-        patCounter = patCounter + 1
-
+    window.bgcolor("blue")
+    #Create the turtle brad - Draws a square
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("white")
+    brad.speed(1)
+    for i in range(1,37):
+        draw_circle(brad)
+        brad.right(10)
+        
     window.exitonclick()
 
-draw_square()
+draw_art()
